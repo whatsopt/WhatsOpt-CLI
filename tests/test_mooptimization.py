5d6b5977246d8d910284e30b7c2bc848d905fac8
relf <remi.lafage@onera.fr>
2022-06-10 11:16:38 +0200

    Skip test used only in development env

--- tests/test_mooptimization.py
+++ tests/test_mooptimization.py
@@ -3,6 +3,7 @@ from whatsopt.mooptimization import MOOptimization, FLOAT, INT
 
 
 class TestMOOptimization(unittest.TestCase):
+    @unittest.skip("Need WhatsOpt connection, only used in development environment")
     def test_constructor(self):
         xtypes = [
             {"type": FLOAT, "limits": [0.0, 1.0]},
